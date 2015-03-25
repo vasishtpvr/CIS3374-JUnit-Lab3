dfa69761471a5256da32c46ca30e2f04c479f797
tuf81059 <tuf81059@CIS-SE35909.tu.temple.edu>
2015-03-25 10:43:35 -0400

    No testing done

---
+++ Lab3/src/test/java/com/lab3/admiterator/AdmiteratorTest.java
@@ -0,0 +1,21 @@
+package com.lab3.admiterator;
+import junitparams.JUnitParamsRunne;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(JUnitParamsRunner.class)
+public class AdmiteratorTest {
+	@Test
+	@parameters(method=admit)
+	
+	public void admitTest(int speed, int accuracy){
+		
+		
+		
+		
+	}
+	private Object admit(){}
+	
+		
+}
